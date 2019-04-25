81092667dbe2661b742ef70498d4aed2f403010f
bluesDD <dd11blues@gmail.com>
2019-04-25 21:21:22 +0900

    Sucseeded to get comment on show

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -3,7 +3,10 @@
 @section('content')
     <div class="container mt-4">
         <div class="border p-4">
+            
+            
                 <div class="mb-4 text-right">
+                    
                     <a class="btn btn-primary" href="{{ route('posts.edit', ['post' => $post]) }}">
                         編集する
                     </a>
@@ -17,7 +20,15 @@
                         <button class="btn btn-danger">削除する</button>
                     </form>
                 </div>
-                
+         
+                @forelse ($post->tags as $tag)
+                <span class="badge badge-secondary">
+                    {{ $tag->tagname }}
+                </span>    
+                @empty
+                @endforelse
+                <br>
+                <br>
             <h1 class="h5 mb-4">
                 {{ $post->title }}
             </h1>
@@ -26,57 +37,65 @@
                 {!! nl2br(e($post->hody)) !!}
             </p>
 
-            <section>
-                <h2 class="h5 mb-4">
-                    コメント
-                </h2>
-                <form class="mb-4" method="POST" action="{{ route('comments.store') }}">
-                    @csrf
-                
-                    <input
-                        name="post_id"
-                        type="hidden"
-                        value="{{ $post->id }}"
-                    >
-                
-                    <div class="form-group">
-                        <label for="body">
-                            本文
-                        </label>
-                
-                        <textarea
-                            id="body"
-                            name="body"
-                            class="form-control {{ $errors->has('body') ? 'is-invalid' : '' }}"
-                            rows="4"
-                        >{{ old('body') }}</textarea>
-                        @if ($errors->has('body'))
-                            <div class="invalid-feedback">
-                                {{ $errors->first('body') }}
-                            </div>
-                        @endif
-                    </div>
-                
-                    <div class="mt-4">
-                        <button type="submit" class="btn btn-primary">
-                            コメントする
-                        </button>
-                    </div>
-                </form>
+       
+            <br>
+            <br>
+            <div>
 
-                @forelse($post->comments as $comment)
-                    <div class="border-top p-4">
-                        <time class="text-secondary">
-                            {{ $comment->created_at->format('Y.m.d H:i') }}
-                        </time>
-                        <p class="mt-2">
-                            {!! nl2br(e($comment->body)) !!}
-                        </p>
-                    </div>
-                @empty
-                    <p>コメントはまだありません。</p>
-                @endforelse
-            </section>
+                <section>
+                    <h2 class="h5 mb-4">
+                        コメント
+                    </h2>
+                    <form class="mb-4" method="POST" action="{{ route('comments.store') }}">
+                        @csrf
+                    
+                        <input
+                            name="post_id"
+                            type="hidden"
+                            value="{{ $post->id }}"
+                        >
+                    
+                        <div class="form-group">
+                            <label for="body">
+                                本文
+                            </label>
+                    
+                            <textarea
+                                id="body"
+                                name="body"
+                                class="form-control {{ $errors->has('body') ? 'is-invalid' : '' }}"
+                                rows="4"
+                            >{{ old('body') }}</textarea>
+                            @if ($errors->has('body'))
+                                <div class="invalid-feedback">
+                                    {{ $errors->first('body') }}
+                                </div>
+                            @endif
+                        </div>
+                    
+                        <div class="mt-4">
+                            <button type="submit" class="btn btn-primary">
+                                コメントする
+                            </button>
+                        </div>
+                    </form>
+    
+                    @forelse($post->comments as $comment)
+                        <div class="border-top p-4">
+                            <time class="text-secondary">
+                                {{ $comment->created_at->format('Y.m.d H:i') }}
+                            </time>
+                            <p class="mt-2">
+                                {!! nl2br(e($comment->body)) !!}
+                            </p>
+                        </div>
+                    @empty
+                        <p>コメントはまだありません。</p>
+                    @endforelse
+                </section>
+
+
+            </div>
         </div>
     </div>
 @endsection
